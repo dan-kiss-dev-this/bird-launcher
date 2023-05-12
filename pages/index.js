04abfd6a096f95301332595dfce451b80da59138
Daniel Kiss <danielkiss@MacBook-Pro.local>
2023-05-12 12:45:47 -0700

    add packages

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
       <main className={`${styles.main} ${inter.className}`}>
         <div className={styles.description}>
           <p>
-            Get started by editing&nbsp;
+            Get started by editing here&nbsp;
             <code className={styles.code}>pages/index.js</code>
           </p>
           <div>
